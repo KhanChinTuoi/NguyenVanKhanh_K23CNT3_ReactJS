343ae8cf22c155b4c6724d9c9e20e0a967422294
Admin <khanhvank9t727@gmail.com>
2025-02-10 16:01:09 +0700

    NVK-ReactJS

---
+++ Day01/dest-demo.js
@@ -0,0 +1,23 @@
+var array = [11,22,33,44];
+var [x,y]=array; //x=11,y=22
+console.log("x=",x);
+console.log("y=",y);
+
+var [a,b,c]= array;
+console.log("a=",a);
+console.log("b=",b);
+console.log("c=",c);
+
+
+
+///object
+const person = {
+    name: "Tony",
+    age: 35,
+    city: "Guangdong"
+};
+
+const {name,city}=person;
+console.log(person.name);
+//console.log(person.age);
+console.log(person.city);
