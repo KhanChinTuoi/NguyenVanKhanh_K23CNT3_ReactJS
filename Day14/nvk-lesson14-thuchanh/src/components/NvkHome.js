610fd99156db61f62927dc5fe932911d43133761
Admin <khanhvank9t727@gmail.com>
2025-03-19 20:29:03 +0700

    NVK-ReactJS

--- Day14/nvk-lesson14-thuchanh/src/components/NvkHome.js
+++ Day14/nvk-lesson14-thuchanh/src/components/NvkHome.js
@@ -1,2 +1,2 @@
-const NvkHome = () => <h1 className="title">Nguyen Van Khanh - Mini Project</h1>;
+const NvkHome = () => <h1 className="text-center" >Nguyen Van Khanh - Mini Project</h1>;
 export default NvkHome;
